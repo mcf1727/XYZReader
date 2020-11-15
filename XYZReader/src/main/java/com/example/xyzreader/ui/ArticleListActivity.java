61bd59a2caa3dc33d80faf488a203b93e0edf2cf
Kev <54264775+mcf1727@users.noreply.github.com>
2020-11-15 18:42:58 +0100

    refactor: CoordinatorLayout/AppBarLayout for MainActivity

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -62,9 +62,8 @@ public class ArticleListActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
+//        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+//        final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
 
